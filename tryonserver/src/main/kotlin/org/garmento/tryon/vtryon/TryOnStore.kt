31110dfa0cc467ce75ad4224bc6a1d842595690a
binhdoitsme <binhdoitsme@gmail.com>
2024-03-24 10:06:29 +0700

    Try-On API: clean up experimental code in favour of new design

--- tryonserver/src/main/kotlin/org/garmento/tryon/vtryon/TryOnStore.kt
+++
@@ -1,9 +0,0 @@
-package org.garmento.tryon.vtryon
-
-import java.io.InputStream
-import java.net.URL
-
-interface TryOnStore {
-    fun save(content: InputStream, id: ImageId = ImageId()): ImageId
-    fun getPublicURL(imageId: ImageId): URL
-}
