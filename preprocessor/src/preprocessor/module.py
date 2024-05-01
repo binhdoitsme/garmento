d20e6c9a5d9c0e67598aed0902677100c6750a96
binhdoitsme <binhdoitsme@gmail.com>
2024-05-01 11:46:11 +0700

    [Preprocessor] Add pyproject.toml, dockerfile, compose file

--- preprocessor/src/preprocessor/module.py
+++ preprocessor/src/preprocessor/module.py
@@ -6,6 +6,7 @@ from injector import Binder, Injector, Module, provider, singleton
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 
+from .api.health_router import HealthCheckRouter
 from .api.router import PreprocessingRouter
 from .api.static_images_router import ImagesRouter
 from .db.job_repository_sqla import JobRepositoryOnSQLA
@@ -33,11 +34,15 @@ class ProductionModule(Module):
     @singleton
     @provider
     def provide_fastapi_app(
-        self, router: PreprocessingRouter, images_router: ImagesRouter
+        self,
+        router: PreprocessingRouter,
+        images_router: ImagesRouter,
+        health_check_router: HealthCheckRouter,
     ) -> FastAPI:
         app = FastAPI()
         app.include_router(router.router)
         app.include_router(images_router.router)
+        app.include_router(health_check_router.router)
         return app
 
 
