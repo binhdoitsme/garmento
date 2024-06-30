00bee2452a58bb1991dba59e8b41020e41944f10
binhdoitsme <binhdoitsme@gmail.com>
2024-07-01 00:43:04 +0700

    [Asset Manager] Support asset upsertion

--- asset-manager/src/asset_manager/storage.py
+++ asset-manager/src/asset_manager/storage.py
@@ -1,5 +1,5 @@
 import os
-from typing import Any, Generator, Optional, Protocol, Union
+from typing import Any, BinaryIO, Generator, Optional, Protocol, Union
 from uuid import UUID, uuid4
 
 from fastapi import UploadFile
@@ -7,7 +7,7 @@ from PIL import Image  # type: ignore
 
 
 class BaseStorageEngine(Protocol):
-    def save_file(self, file: UploadFile) -> str: ...
+    def save_file(self, file: BinaryIO, id: str | None = None) -> str: ...
 
     def get_file(
         self, asset_id: UUID
@@ -15,11 +15,11 @@ class BaseStorageEngine(Protocol):
 
 
 class FileStorageEngine(BaseStorageEngine):
-    def save_file(self, file: UploadFile) -> str:
-        asset_id = uuid4()
+    def save_file(self, file: BinaryIO, id: str | None = None) -> str:
+        asset_id = uuid4() if not id else UUID(id)
         if not os.path.exists("assets"):
             os.mkdir("assets")
-        image = Image.open(file.file)
+        image = Image.open(file)
         image.thumbnail((2048, 2048))
         image.save(f"assets/{asset_id}.jpg")
         return str(asset_id)
--- asset-manager/src/main.py
+++ asset-manager/src/main.py
@@ -1,6 +1,9 @@
 import logging
+from io import BytesIO
 from uuid import UUID
 
+import aiohttp
+from pydantic import BaseModel
 from asset_manager.storage import FileStorageEngine
 from fastapi import FastAPI, File, HTTPException, UploadFile
 from fastapi.responses import StreamingResponse
@@ -19,8 +22,26 @@ async def health_check():
 
 @app.post("/assets")
 async def upload_image(file: UploadFile = File(...)):
-    image_id = storage_engine.save_file(file)
+    image_id = storage_engine.save_file(file.file)
     return {"id": image_id, "url": f"/assets/{image_id}"}
+
+
+class UpsertImageRequest(BaseModel):
+    id: str
+    url: str
+
+
+@app.post("/assets/upsert")
+async def upsert_image(request: UpsertImageRequest):
+    id = request.id
+    url = request.url.replace("/api/model-api", "http://model-api")
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            response.raise_for_status()
+            body = await response.read()
+            file = BytesIO(body)
+            image_id = storage_engine.save_file(file, id)
+            return {"id": image_id, "url": f"/assets/{image_id}"}
 
 
 @app.get("/assets/{asset_id}")
